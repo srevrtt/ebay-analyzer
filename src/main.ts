17a7a7e58bb2f316b81541367adc0abcec6c48e9
srevrtt <everettsistrunk@icloud.com>
2025-03-08 10:34:24 -0700

    fishing out raw pricing data

--- src/main.ts
+++ src/main.ts
@@ -15,13 +15,55 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    const lines = input.value.split('\n');
-    let lineIdx = 0; // This allows us to skip ahead when we need to.
+    const lines = input.value.toLowerCase().split('\n');
+
+    let filteredLines = [];
+
+    // This is so that we know when to look for pricing data because
+    // we need to filter out gunk at the beginning that we don't need.
+    let lookForPricing = false;
 
     let prices = [];
     let shippingPrices = [];
 
-    // Let's first start with the standard prices.
-    // ...
+    // Let's filter out all of the unnecessary text.
+    // All we need is the pricing & shipping data.
+    lines.forEach((value) => {
+      if (value.indexOf(`$`) >= 0) {
+        // Filter out the price range filter
+        if (value.indexOf('under') >= 0 || value.indexOf('over') >= 0) {
+          return;
+        }
+
+        // Filter out % off sales
+        if (value.indexOf('% off') >= 0) {
+          return;
+        }
+
+        // Filter out shipping *estimates* -- we want the exact numbers
+        if (value.indexOf('shipping estimate') >= 0) {
+          return;
+        }
+
+        // Filter out lines that just contain '$'
+        if (value.length == 1) {
+          return;
+        }
+
+        // We need to wait until we see a delivery cost...
+        // This is because we need to filter out the beginning part of the
+        // text, which includes the price range filter, which is problematic.
+
+        if (value.indexOf('delivery') >= 0 && !lookForPricing) {
+          lookForPricing = true;
+          return;
+        }
+
+        if (lookForPricing) {
+          filteredLines.push(value);
+          console.log(value);
+        }
+      }
+    });
   });
 });
